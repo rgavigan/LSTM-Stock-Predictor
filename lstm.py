615bc9111fed1b42d1744970063831936c4b412c
rgavigan <rileygav@hotmail.com>
2022-04-29 12:00:30 -0400

    Standard model and exponential completed, LSTM in progress

--- lstm.py
+++ lstm.py
@@ -95,3 +95,213 @@ for ti in range(250):
   EMA = gamma*train_data[ti] + (1-gamma)*EMA
   train_data[ti] = EMA
 all_mid_data = np.concatenate([train_data,test_data],axis=0)
+
+
+# Standard Average
+window_size = 100
+N = train_data.size
+std_avg_predictions = []
+std_avg_x = []
+mse_errors = []
+
+for pred_idx in range(window_size,N):
+
+    if pred_idx >= N:
+        date = dt.datetime.strptime(k, '%Y-%m-%d').date() + dt.timedelta(days=1)
+    else:
+        date = df.loc[pred_idx,'Date']
+
+    std_avg_predictions.append(np.mean(train_data[pred_idx-window_size:pred_idx]))
+    mse_errors.append((std_avg_predictions[-1]-train_data[pred_idx])**2)
+    std_avg_x.append(date)
+
+print('MSE error for standard averaging: %.5f'%(0.5*np.mean(mse_errors)))
+
+# Plot the averaged results
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+plt.plot(range(window_size,N),std_avg_predictions,color='orange',label='Prediction')
+#plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+plt.xlabel('Date')
+plt.ylabel('Mid Price')
+plt.legend(fontsize=18)
+plt.show()
+
+
+# Exponential Moving Average
+window_size = 100
+N = train_data.size
+
+run_avg_predictions = []
+run_avg_x = []
+
+mse_errors = []
+
+running_mean = 0.0
+run_avg_predictions.append(running_mean)
+
+decay = 0.5
+
+for pred_idx in range(1,N):
+
+    running_mean = running_mean*decay + (1.0-decay)*train_data[pred_idx-1]
+    run_avg_predictions.append(running_mean)
+    mse_errors.append((run_avg_predictions[-1]-train_data[pred_idx])**2)
+    run_avg_x.append(date)
+
+print('MSE error for EMA averaging: %.5f'%(0.5*np.mean(mse_errors)))
+
+
+# Plot the exponential moving average
+plt.figure(figsize = (18,9))
+plt.plot(range(df.shape[0]),all_mid_data,color='b',label='True')
+plt.plot(range(0,N),run_avg_predictions,color='orange', label='Prediction')
+plt.xticks(range(0,df.shape[0],50),df['Date'].loc[::50],rotation=45)
+plt.xlabel('Date')
+plt.ylabel('Mid Price')
+plt.legend(fontsize=18)
+plt.show()
+
+#Long Short-Term Model
+
+# Creation of batch of data
+class DataGeneratorSeq(object):
+
+    def __init__(self,prices,batch_size,num_unroll):
+        self._prices = prices
+        self._prices_length = len(self._prices) - num_unroll
+        self._batch_size = batch_size
+        self._num_unroll = num_unroll
+        self._segments = self._prices_length //self._batch_size
+        self._cursor = [offset * self._segments for offset in range(self._batch_size)]
+
+    def next_batch(self):
+
+        batch_data = np.zeros((self._batch_size),dtype=np.float32)
+        batch_labels = np.zeros((self._batch_size),dtype=np.float32)
+
+        for b in range(self._batch_size):
+            if self._cursor[b]+1>=self._prices_length:
+                #self._cursor[b] = b * self._segments
+                self._cursor[b] = np.random.randint(0,(b+1)*self._segments)
+
+            batch_data[b] = self._prices[self._cursor[b]]
+            batch_labels[b]= self._prices[self._cursor[b]+np.random.randint(0,5)]
+
+            self._cursor[b] = (self._cursor[b]+1)%self._prices_length
+
+        return batch_data,batch_labels
+
+    def unroll_batches(self):
+
+        unroll_data,unroll_labels = [],[]
+        init_data, init_label = None,None
+        for ui in range(self._num_unroll):
+
+            data, labels = self.next_batch()    
+
+            unroll_data.append(data)
+            unroll_labels.append(labels)
+
+        return unroll_data, unroll_labels
+
+    def reset_indices(self):
+        for b in range(self._batch_size):
+            self._cursor[b] = np.random.randint(0,min((b+1)*self._segments,self._prices_length-1))
+
+
+
+dg = DataGeneratorSeq(train_data,5,5)
+u_data, u_labels = dg.unroll_batches()
+
+for ui,(dat,lbl) in enumerate(zip(u_data,u_labels)):   
+    print('\n\nUnrolled index %d'%ui)
+    dat_ind = dat
+    lbl_ind = lbl
+    print('\tInputs: ',dat )
+    print('\n\tOutput:',lbl)
+
+# Defining Hyperparameters
+D = 1 # Dimensionality of the data. Since your data is 1-D this would be 1
+num_unrollings = 50 # Number of time steps you look into the future.
+batch_size = 500 # Number of samples in a batch
+num_nodes = [200,200,150] # Number of hidden nodes in each layer of the deep LSTM stack we're using
+n_layers = len(num_nodes) # number of layers
+dropout = 0.2 # dropout amount
+
+tf.compat.v1.reset_default_graph() # This is important in case you run this multiple times
+
+# Defining Inputs + Outputs
+train_inputs, train_outputs = [],[]
+tf.compat.v1.disable_eager_execution()
+# You unroll the input over time defining placeholders for each time step
+for ui in range(num_unrollings):
+    train_inputs.append(tf.compat.v1.placeholder(tf.float32, shape=[batch_size,D],name='train_inputs_%d'%ui))
+    train_outputs.append(tf.compat.v1.placeholder(tf.float32, shape=[batch_size,1], name = 'train_outputs_%d'%ui))
+
+# Defining LSTM + Regression Layer Parameters
+lstm_cells = [
+    tf.keras.layers.LSTMCell(units=num_nodes[li])
+ for li in range(n_layers)]
+
+drop_lstm_cells = [tf.nn.RNNCellDropoutWrapper(
+    lstm, input_keep_prob=1.0,output_keep_prob=1.0-dropout, state_keep_prob=1.0-dropout
+) for lstm in lstm_cells]
+drop_multi_cell = tf.keras.layers.StackedRNNCells(drop_lstm_cells)
+multi_cell = tf.keras.layers.StackedRNNCells(lstm_cells)
+
+w = tf.compat.v1.get_variable('w',shape=[num_nodes[-1], 1], initializer=tf.keras.initializers.glorot_normal)
+b = tf.compat.v1.get_variable('b',initializer=tf.compat.v1.random_uniform([1],-0.1,0.1))
+
+
+# Calculating the LSTM Output, Feed to Regression Layer, Final Prediction
+# Create cell state and hidden state variables to maintain the state of the LSTM
+c, h = [],[]
+initial_state = []
+for li in range(n_layers):
+  c.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+  h.append(tf.Variable(tf.zeros([batch_size, num_nodes[li]]), trainable=False))
+  initial_state.append(tf.compat.v1.nn.rnn_cell.LSTMStateTuple(c[li], h[li]))
+
+# Do several tensor transofmations, because the function dynamic_rnn requires the output to be of
+# a specific format. Read more at: https://www.tensorflow.org/api_docs/python/tf/nn/dynamic_rnn
+all_inputs = tf.concat([tf.expand_dims(t,0) for t in train_inputs],axis=0)
+
+# all_outputs is [seq_length, batch_size, num_nodes]
+all_lstm_outputs, state = tf.keras.layers.RNN(
+    drop_multi_cell, all_inputs, time_major = True)
+
+all_lstm_outputs = tf.reshape(all_lstm_outputs, [batch_size*num_unrollings,num_nodes[-1]])
+
+all_outputs = tf.nn.xw_plus_b(all_lstm_outputs,w,b)
+
+split_outputs = tf.split(all_outputs,num_unrollings,axis=0)
+
+
+# Loss Calculation and Optimization
+print('Defining training Loss')
+loss = 0.0
+with tf.control_dependencies([tf.assign(c[li], state[li][0]) for li in range(n_layers)]+
+                             [tf.assign(h[li], state[li][1]) for li in range(n_layers)]):
+  for ui in range(num_unrollings):
+    loss += tf.reduce_mean(0.5*(split_outputs[ui]-train_outputs[ui])**2)
+
+print('Learning rate decay operations')
+global_step = tf.Variable(0, trainable=False)
+inc_gstep = tf.assign(global_step,global_step + 1)
+tf_learning_rate = tf.placeholder(shape=None,dtype=tf.float32)
+tf_min_learning_rate = tf.placeholder(shape=None,dtype=tf.float32)
+
+learning_rate = tf.maximum(
+    tf.train.exponential_decay(tf_learning_rate, global_step, decay_steps=1, decay_rate=0.5, staircase=True),
+    tf_min_learning_rate)
+
+# Optimizer.
+print('TF Optimization operations')
+optimizer = tf.train.AdamOptimizer(learning_rate)
+gradients, v = zip(*optimizer.compute_gradients(loss))
+gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+optimizer = optimizer.apply_gradients(
+    zip(gradients, v))
+
+print('\tAll done')
